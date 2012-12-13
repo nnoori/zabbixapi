37f8faad1285afe323959d50deb353535fbbc6ee
nadia Noori <nnoori@coralcea.ca>
2012-12-13 09:20:03 -0500

    Zabbix API calls test, with modified values .. the calls in that file do not match the calls in /lib/zabbixapi/hosts.rb

---
+++ lib/ApiTest.rb
@@ -0,0 +1,43 @@
+require "zabbixapi"
+
+#connecto to the Zabbix server
+zbx = ZabbixApi.connect(:url => 'http://zabbix2/zabbix/api_jsonrpc.php',:user => 'Admin', :password => 'zabbix')
+
+#create a host 
+#host_id = zbx.hosts.add(
+#	:host => "testAPIHOST", 
+#	:interfaces => [
+#		:type => 1, 
+#		:main => 1, 
+#		:useip => 1, 
+#		:ip => "192.168.1.231", 
+#		:dns =>"", 
+#		:port => 10050], 
+#	:groups => [:groupid => zbx.hostgroups.get_id(:name => "Linux servers")])
+#puts host_id
+#host_jmx_id = zbx.hosts.add(
+#	:host => "testAPIJMX", 
+#	:interfaces => [
+#		:type => 4, 
+#		:main => 1, 
+#		:useip => 1, 
+#		:ip => "192.168.1.231", 
+#		:dns =>"", 
+#		:port => 7091], 
+#		:groups => [:groupid => zbx.hostgroups.get_id(:name => "Linux servers")])
+#puts host_jmx_id
+#enable the host 
+#to enable monitoring the host we need to set the "status" flag to 0
+zbx.hosts.update(:hostid => zbx.hosts.get_id(:host => "testAPIJMX"), :status =>0)
+
+#delete  
+#create an application
+#zbx.applications.create(:name => "testApplication", :hostid => "10091")
+
+#delete an item 
+
+
+#create an item
+
+
+#delete an item  
